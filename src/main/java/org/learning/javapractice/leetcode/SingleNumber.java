9454457e9d8a4dcd4bd6dadd8f8c1f1a3b8b4b0d
jeeten <j.jeeten@gmail.com>
2022-01-14 22:36:06 +0530

    Initial pom.xml

--- src/main/java/org/learning/javapractice/leetcode/SingleNumber.java
+++ src/main/java/org/learning/javapractice/leetcode/SingleNumber.java
@@ -6,8 +6,8 @@ import java.util.Set;
 public class SingleNumber {
     public static void main(String[] args) {
         SingleNumber service = new SingleNumber();
-        int[] nums = new int[]{4,1,2,1,2};
-        System.out.println(service.singleNumber(nums));
+        int[] nums = new int[]{4, 1, 2, 1, 2};
+        System.out.println(service.singleNumber1(nums));
     }
 
     public int singleNumber(int[] nums) {
@@ -24,9 +24,9 @@ public class SingleNumber {
 
     public int singleNumber1(int[] nums) {
         int result = 0;
-        for(int num: nums) {
-            System.out.println("");
+        for (int num : nums) {
             result ^= num;
+            System.out.println("");
         }
         return result;
     }
